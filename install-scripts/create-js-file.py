42429d359fe8738b9d6fd8909867b41782b8e41d
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2020-10-08 18:58:41 -0400

    Lorem Ipsum
    Lorrem ipsum dolor

--- install-scripts/create-js-file.py
+++ install-scripts/create-js-file.py
@@ -2,6 +2,7 @@ import os
 def bpm_createfile(filename, data):
   fil = open(filename + ".js", "w")
   fil.write(data)
+  fil.close()
 def bpm_deletefile(filename):
   if os.path.exists(filename + ".js"):
     os.remove(filename + ".js")
